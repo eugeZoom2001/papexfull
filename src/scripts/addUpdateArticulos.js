dfecabb602d0f9a18357eabfe7ef95a4ff0b4b4e
euge <eugenionoche2@gmail.com>
2024-09-25 15:54:10 -0300

    secon commit

--- src/scripts/addUpdateArticulos.js
+++ src/scripts/addUpdateArticulos.js
@@ -1,20 +1,41 @@
 const inputBarras = $("#inputBarras")
 const inputSku = $("#inputSku")
+let resultSku = null
+let id_articuloSelect = null
 
 
 $(document).ready(function () {
     // console.log("urlBarras", urlBarras);
     // console.log("urlBase  ", urlBase);
+
     $('#inputBarras').focus()
-    $('#loadButton').click(function () {
-        $('#overlay').show(); // Mostrar el overlay
-        $('#spinner').show(); // Mostrar el spinner
-
-        // Simular un proceso de carga
-        // setTimeout(function () {
-        //     $('#overlay').hide(); // Ocultar el overlay
-        //     $('#spinner').hide(); // Ocultar el spinner
-        // }, 2000); // Cambia 2000 por el tiempo que desees
-
+    $('#loadButton').click(async function () {
+        if (inputBarras.val()) {
+            resultSku = await getSkus()
+            if (resultSku) {
+                console.log(resultSku);
+            } else {
+                console.log("error"); //no hay resultados
+            }
+        }
     })
 })
+
+const openSpinner = () => {
+    $('#overlay').show(); // Mostrar el overlay
+    $('#spinner').show(); // Mostrar el spinner   
+}
+
+const closeSpinner = () => {
+    $('#overlay').hide(); // Mostrar el overlay
+    $('#spinner').hide(); // Mostrar el spinner   
+}
+
+const getSkus = async () => {
+    const url = `${urlBarras}${inputBarras.val()}`
+    const res = await axios.get(url);
+    if (res.data.result === "ok") resultSku = res.data.data
+    else resultSku = null
+
+
+}
