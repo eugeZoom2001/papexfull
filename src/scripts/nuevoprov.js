df8a298c442f3467bff9e79350c627f3450c85ea
euge <eugenionoche2@gmail.com>
2024-10-01 23:23:55 -0300

    alta proveedor A

---
+++ src/scripts/nuevoprov.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $("#formProv").submit(function (e) {
+    e.preventDefault();
+    getForm();
+  });
+  $("input").click(function (e) {
+    e.target.value = "";
+  });
+});
+
+const getForm = async () => {
+  let formData = new FormData();
+  formData.append("nombre", $("#idNombre").val());
+  formData.append("referente", $("#idReferente").val());
+  formData.append("telefono", $("#idTelefono").val());
+  formData.append("direccion", $("#idDireccion").val());
+  formData.append("email", $("#idEmail").val());
+  sendData(formData);
+};
+const sendData = async (formData) => {
+  axios({
+    method: "post",
+    url: urlProveedores,
+    data: formData,
+    headers: { "Content-Type": "application/json" },
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (response) {
+      console.log(response);
+    });
+};
